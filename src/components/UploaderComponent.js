801a558e8de697e789818fb7cb0c20205b15d98f
Zuzki <ldegrac071@hotmail.com>
2020-09-16 08:04:03 -0500

    Dynamic rendering done
    I use ternary operator to render different components
    
    added await/async to force state update

--- src/components/UploaderComponent.js
+++ src/components/UploaderComponent.js
@@ -8,6 +8,7 @@ export default class Uploader extends Component {
     super(props)
 
     this.state = {
+      render: true,
       progress: 0,
       image: null,
       error: "",
@@ -26,11 +27,11 @@ export default class Uploader extends Component {
     });
   }
 
-  componentDidUpdate() {
-    console.log(this.state)
-  }
-
   fileSelectedHandler = async (event) => {
+
+    this.setState({
+      render: false
+    })
 
     const file = event.target.files[0];
     
@@ -41,9 +42,9 @@ export default class Uploader extends Component {
       if( validTypes.includes(fileType) ){
         await this.setState({ error: "" });
         await this.setState({ ...this.state, image: file });
-      }else await this.setState({ error: "Incorrect file type"})
+      }else return await this.setState({ error: "Incorrect file type"})
 
-    }else await this.setState({ error: "No file were selected"})
+    }else return await this.setState({ error: "No file were selected"})
 
     if (this.state.image) {
       const storageRef = storage.ref(`imagenes/${this.state.image.name}`);
@@ -51,11 +52,14 @@ export default class Uploader extends Component {
 
       task.on(
         "state_changed",
-        async (snapshot) => {
-          const progress = Math.round(
-            (snapshot.bytesTransferred / snapshot.totalBytes) * 100
-          );
-          await this.setState({ progress: progress });
+        (snapshot) => {
+          var progress = 0
+          if( snapshot.metadata){
+            for (let i = 0; i <= 100; i+= 0.5) {
+                progress = i
+                this.setState({ progress: progress });
+            }
+          }
         },
         async (err) => await this.setState({ error: err }),
         () => {
@@ -63,20 +67,30 @@ export default class Uploader extends Component {
             .ref("imagenes")
             .child(this.state.image.name)
             .getDownloadURL()
-            .then( async (url) => {
-              await this.setState({ url: url });
-              await this.setState({ progress: 0 });
+            .then( (url) => {
+              this.setState({ url: url });
+              this.setState({ progress: 0 });
             });
         }
       );
-    } else await this.setState({ error: "No image available" });
+    } else return await this.setState({ error: "No image available" });
   }
 
   render(){
     return (
       <Fragment>
-        <DragDrop upload={ this.fileSelectedHandler } />
+        {this.state.render ? (
+          <DragDrop upload={this.fileSelectedHandler} />
+        ) : this.state.error ? (
+          <p style={{ color: "red", margin: "0 auto" }}>{this.state.error}</p>
+        ) : this.state.progress > 0 ? (
+          <progress style={{ margin: "0 auto" }}></progress>
+        ) : this.state.url ? (
+          <p style={{ margin: "0 auto" }}>{this.state.url}</p>
+        ) : (
+          <div></div>
+        )}
       </Fragment>
-    )
+    );
   }
 }
